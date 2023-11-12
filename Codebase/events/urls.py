c68cdc78523698a68bbdf2efb2e2783c4587dee5
Jason Ngu <jason.ngu058@gmail.com>
2023-11-12 13:31:20 -0700

    Creation of Calendar Interface in HTML for later use

--- Codebase/events/urls.py
+++ Codebase/events/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"), # point to home function in views.py
+    # Path Convertors
+    # int: numbers
+    # str: strings
+    # path: whole URLS
+    # slug: hyphen-and_underscore stuff
+    # UUID: universually unique identifier
+
+    # path('', views.home, name="home"), # point to home function in views.py
+
+    path('<int:year>/<str:month>/', views.home, name="home"),
 ]
--- Codebase/events/views.py
+++ Codebase/events/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
-# Create your views here.
 
-def home(request):
-    return render(request, 'home.html', {})
+
+def home(request, year, month):
+    name = "John"
+    month = month.title() # Make first letter capital
+
+    #convert month from name to num
+    month_num = int(list(calendar.month_name).index(month))
+
+    # create a cealendar
+    cal = HTMLCalendar().formatmonth(int(year), month_num)
+
+    # get current year
+    now = datetime.now()
+    curr_year = now.year
+
+    # get current time -- NOT MST MAYBE GWT?
+    curr_time = now.strftime('%I:%M %p')
+
+
+    return render(request,           
+        'home.html', {
+            "name": name,
+            "year": year,
+            "month": month,
+            "month_num": month_num,
+            "cal": cal,
+            "curr_year": curr_year,
+            "curr_time": curr_time,
+        })
