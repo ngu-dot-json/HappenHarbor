72a1bd3a7932802649f4a3b13e874cd308ce4db0
Jason Ngu <jason.ngu058@gmail.com>
2023-11-13 16:55:02 -0700

    further codebase adjustments and introduction of about page

--- Codebase/events/urls.py
+++ Codebase/events/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
 
     path('', views.home, name="home"), # point to home function in views.py
     path('<int:year>/<str:month>/', views.home, name="home"),
-    # path('<int:year>/<str:month>/', views.calendar, name="calendar"),
     path('events', views.all_events, name="list-events"),
+    path('about', views.about, name="about"),
 ]
--- Codebase/events/views.py
+++ Codebase/events/views.py
@@ -5,6 +5,17 @@ from datetime import datetime
 from .models import Event
 
 
+
+def about(request):
+    foo = "bar"
+
+    return render(request, 'events/about.html', {})
+
+
+
+
+
+
 # Import data from Django Database
 def all_events(request):
     event_list = Event.objects.all()
@@ -15,16 +26,14 @@ def all_events(request):
          })
 
 
-
-
 # shows default current month and year calendar
 def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
     name = "John"
     month = month.title() # Make first letter capital
 
     #convert month from name to num
-    # month_num = int(list(calendar.month_name).index(month))
-    month_num = 12
+    month_num = int(list(calendar.month_name).index(month))
+    # month_num = 12
 
     # create a cealendar
     cal = HTMLCalendar().formatmonth(int(year), month_num)
@@ -46,34 +55,3 @@ def home(request, year=datetime.now().year, month=datetime.now().strftime('%B'))
             "curr_year": curr_year,
             "curr_time": curr_time,
         })
-
-
-def calendar(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
-    name = "John"
-    month = month.title() # Make first letter capital
-
-    #convert month from name to num
-    # month_num = int(list(calendar.month_name).index(month))
-    month_num = 12
-
-    # create a cealendar
-    cal = HTMLCalendar().formatmonth(int(year), month_num)
-
-    # get current year
-    now = datetime.now()
-    curr_year = now.year
-
-    # get current time -- NOT MST MAYBE GWT?
-    curr_time = now.strftime('%I:%M %p')
-
-    return render(request,           
-        'events/home.html', {
-            "name": name,
-            "year": year,
-            "month": month,
-            "month_num": month_num,
-            "cal": cal,
-            "curr_year": curr_year,
-            "curr_time": curr_time,
-        })
-
