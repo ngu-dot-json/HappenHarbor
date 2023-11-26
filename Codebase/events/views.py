e113d20db36261150901d26857b29cd4d28b39f4
Jason Ngu <jason.ngu058@gmail.com>
2023-11-26 15:10:38 -0700

    initial setup for account login feature

--- Codebase/events/views.py
+++ Codebase/events/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
 from .models import Event
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout
+from django.shortcuts import HttpResponseRedirect
 
 def about(request):
     foo = "bar"
@@ -13,7 +19,6 @@ def account(request):
     foo = "bar"
 
     return render(request, 'events/account.html', {})
-
 
 
 # Import data from Django Database
@@ -64,3 +69,48 @@ def calendar(request, year=datetime.now().year, month=datetime.now().strftime('%
             "curr_year": curr_year,
             "curr_time": curr_time,
         })
+
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'signup.html', {'form': form})
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
+  
+def signin(request):
+    if request.user.is_authenticated:
+        return render(request, 'home.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/profile') #profile
+        else:
+            msg = 'Error Login'
+            form = AuthenticationForm(request.POST)
+            return render(request, 'login.html', {'form': form, 'msg': msg})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+  
+def profile(request): 
+    return render(request, 'profile.html')
+   
+def signout(request):
+    logout(request)
+    return redirect('/')
